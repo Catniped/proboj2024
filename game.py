b39871dec1e1428c36301a29d8e2557e59ca49b4
Catniped <ferkokrc.5tube@gmail.com>
2024-01-10 00:04:20 +0100

    fullscreen + comments

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import easygame, uihelper
 
-easygame.open_window('window', 800, 600)
+window = easygame.open_window('window', None, None, True)
+"""fsh = window.height
+window.set_fullscreen(False)"""
 
 def testCallback(button):
     print(button)
@@ -8,6 +10,7 @@ def testCallback(button):
 group1 = uihelper.uiGroup()
 uihelper.uiElement(easygame.load_image("testbutton.png"),(400,300),group=group1,callback=testCallback)
 
+fullscreen = False
 should_quit = False
 while not should_quit:
 
@@ -17,6 +20,10 @@ while not should_quit:
             should_quit = True
         if evtype is easygame.MouseDownEvent:
             group1.checkGroup(event)
+        if evtype is easygame.KeyDownEvent:
+            if event.key == "9":
+                fullscreen = not fullscreen
+                window.set_fullscreen(fullscreen)
 
     group1.renderGroup()
 
