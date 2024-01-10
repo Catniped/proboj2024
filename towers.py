43097286cedb81f6fec428c43c1a305b48dc204c
Filip2525 <filip.stefko2525@gmail.com>
2024-01-10 15:03:54 +0100

    towers

--- towers.py
+++ towers.py
@@ -1,6 +1,10 @@
 class Archer:
-    def __init__(self, damage):
-        self.damage = damage
-
+    def __init__(self):
+        self.damage = 20
+        self.speed = 30
+    def shoot(self,enemyxy,enemyspeed,enemyvector):
+        target = (enemyxy[0] + enemyspeed * self.speed * enemyvector[0], enemyxy[1] + enemyspeed * self.speed * enemyvector[1])
 class Mage:
-    pass
+    def __init__(self):
+        self.damage = 20
+        self.speed = 30
