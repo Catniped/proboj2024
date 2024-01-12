f96ab2abb2dfa127a5654179e9e7d3b2cfd30615
Phiton <milosko@weba.sk>
2024-01-12 15:38:53 +0100

    added auto change offset of map by resolution

--- game.py
+++ game.py
@@ -22,6 +22,7 @@ y_scale=window.height/900
 
 def spawnTestEnemy(buttons):
     uihelper.enemyElement(easygame.load_image("Assets/Sprites/UFO/UFO(3).png"),(1362,495),group=enemies,scale=0.3)
+    playButton.image=playIconClick
 
 def toggleShop(buttons):
     if not shopui.enabled:
@@ -45,6 +46,20 @@ def buyTower(buttons):
     
 mapi = easygame.load_image("Assets/Tileset/map.png")
 offset = -(mapi.width*0.2 - window.width*2)
+# print(offset)
+xs=x_scale
+if x_scale==1:
+    xs=0
+
+if y_scale==1:
+    y_scale=0
+
+offset=-76.8*x_scale+100*xs
+offsetY=((116)*(y_scale))
+print(y_scale)
+print(offsetY)
+print(116*(y_scale-1))
+# offsetY=0
 
 ui1 = uihelper.uiGroup()
 shopui = uihelper.uiGroup(visible=False,enabled=False)
@@ -55,10 +70,12 @@ projectiles = uihelper.uiGroup()
 cartIconIdle = easygame.load_image("Assets/Buttons/png/Buttons/Square-Icon-Blue/Cart-Idle.png")
 cartIconActive = easygame.load_image("Assets/Buttons/png/Buttons/Square-Icon-Blue/Cart-Click.png")
 
+playIconIdle = easygame.load_image("Assets/Buttons/png/Buttons/Rect-Icon-Blue/Play-Idle.png")
+playIconClick = easygame.load_image("Assets/Buttons/png/Buttons/Rect-Icon-Blue/Play-Click.png")
 
 cartIcon = uihelper.uiElement(cartIconIdle,(50,window.height-105),group=ui1,scale=1,callback=toggleShop,ui=True)
 uihelper.uiElement(easygame.load_image("Assets/Buttons/png/Dummy/Rect-Icon-Blue/Idle.png"),(window.width-150,window.height-105),group=ui1,scale=1,enabled=False,ui=True)
-uihelper.uiElement(easygame.load_image("Assets/Buttons/png/Buttons/Rect-Icon-Blue/Play-Idle.png"),(100,100),group=ui1,scale=1,callback=spawnTestEnemy,ui=True)
+playButton=uihelper.uiElement(playIconIdle,(100,100),group=ui1,scale=1,callback=spawnTestEnemy,ui=True)
 balanceDisplay = uihelper.textElement(str(balance),"Poppins",30,(window.width-140,window.height-96),ui=True,group=ui1)
 
 
@@ -69,6 +86,8 @@ uihelper.textElement("100","Poppins",30,((315,window.height-290)),ui=True,group=
 
 while not should_quit:
     camera=easygame.get_camera()
+    if playButton.image is playIconClick:
+        playButton.image=playIconIdle
     for event in easygame.poll_events():
         evtype = type(event)
         if evtype is easygame.CloseEvent:
@@ -107,7 +126,7 @@ while not should_quit:
 
     """scene prep"""
     easygame.fill(0.200, 0.230, 0.245)
-    uihelper.drawCentered(mapi,scale=0.15,position=(offset,0),window=window)
+    uihelper.drawCentered(mapi,scale=0.15,position=(offset,offsetY),window=window)
 
     """logic"""
     for enemy in enemies.elements:
